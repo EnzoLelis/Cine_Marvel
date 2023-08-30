e028d47fc73b387233926283e88f30a2a607692f
EnzoLelis <enzo-lelis@hotmail.com>
2023-08-29 22:44:13 -0300

    first

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,32 @@
 import Title from '@/components/Title'
 import Image from 'next/image'
 import Title from './componentes/title'
+import CardFilme from '@/components/CardFilme'
 
 export default function Home() {
+  const filmes = [ 
+  {titulo: "Megatubarão 2",
+  nota: 6.2,
+  poster:"https://www.themoviedb.org/t/p/w220_and_h330_face/8tBhAn6qVRQzf5yvEcxjgPMgTkw.jpg"
+},
+{titulo: "Barbie",
+nota: 7.5,
+poster:"https://www.themoviedb.org/t/p/w94_and_h141_bestv2/yRRuLt7sMBEQkHsd1S3KaaofZn7.jpg"
+
+}
+
+
+  ]
+  const megatubarao ={
+    titulo: "Megatubarão 2",
+    nota: 6.2,
+    poster:"https://www.themoviedb.org/t/p/w220_and_h330_face/8tBhAn6qVRQzf5yvEcxjgPMgTkw.jpg"
+  }
+  const barbie = {
+    titulo: "Barbie",
+    nota: 7.5,
+    poster:"https://www.themoviedb.org/t/p/w94_and_h141_bestv2/yRRuLt7sMBEQkHsd1S3KaaofZn7.jpg"
+  }
   return (
     <>
     <nav className="flex p-4 bg-slate-900">
@@ -10,40 +34,26 @@ export default function Home() {
       <ul className="Flex gap-20">
         <li>
           <a href = "#">
-<<<<<<< HEAD
       <h1>Cine Marvel</h1>
-=======
-      <h1>Fiap App</h1>
->>>>>>> 8491a812a83296748a2a7e00e639c9d291e67300
 
           </a>
         </li>
         <li>
           <a href = "#">
-<<<<<<< HEAD
             filmes
-=======
-            Jogos
->>>>>>> 8491a812a83296748a2a7e00e639c9d291e67300
           </a>
         </li>
       </ul>
     </nav>
 
-<<<<<<< HEAD
     <Title>em alta</Title> 
 
-    <div id="card" className="flex flex-col w-40 justify-center items-center m-2">
-      <img className="rounded" src="https://place-hold.it/150x220/666" alt="" />
-      <span className="font-bold text-center line-clamp-1"></span>titulo do filme do card
-      <div>
-        <span>6.0</span>
-      </div>
-      <a href="#" className= "bg-pink-600 py-2  w-full rounded text-center">
-        detalhes 
-        </a>
-    </div>
-        
+    <section classname="flex flex-wrap">
+      {filmes.map(filme => <CardFilme filme={filme} /> )}
+
+    </section>
+    
+
     <Title>lançamentos</Title>
     <Title>favoritos</Title>
     
@@ -54,21 +64,3 @@ export default function Home() {
     
     
 
-=======
-    <Title> em alta</Title> 
-
-     <div id="card" className="flex flex-col w-40 justify-center items-center m-2">
-        <img className="rounded"src="https://place-hold.it/150x220/666" alt="" />
-        <span className="font-bold text-center line-clamp-1">Clash of Clans</span>
-        <div>
-          <span>6.0</span>
-        </div>
-        <a href="#" className="bg-pink-600 py-2 w-full rounded text-center">detalhes</a>
-     </div>  
-    <Title> lançamento</Title>   
-  
-
-    </>
-  )
-}
->>>>>>> 8491a812a83296748a2a7e00e639c9d291e67300
