d5e284a855d768f7a492aaf3c6d8fba0059531d4
EnzoLelis <enzo-lelis@hotmail.co>
2023-09-12 22:28:43 -0300

    atualizando

--- src/components/CardFilme.jsx
+++ src/components/CardFilme.jsx
@@ -1,19 +1,24 @@
 "use client"
 
+import useFavorito from '@/hooks/filmes'
 import { HeartIcon } from '@heroicons/react/24/outline'
 import { useState } from 'react'
 
 export default function CardFilme({filme}){
-    const [favorito, setFavorito] = useState(true)
+    const {favorito, desfavoritar, favoritar} = useFavorito()
+
     const url_imagem = `https://image.tmdb.org/t/p/w200/${filme.poster_path}`
+
+
+
 
 
     return(
         <div id="card" className="flex flex-col w-40 justify-center items-center m-2">
          { favorito ?  
-           <HeartIcon onCLick={() => setFavorito(false)} className="h-6 w-6 text-rose-600" /> 
+           <HeartIcon onCLick={() => desfavoritar(filme)} className="h-6 w-6 text-rose-600" /> 
            :
-           <HeartIcon onCLick={() => setFavorito(true)} className="h-6 w-6 text-slate-100" /> 
+           <HeartIcon onCLick={() => favoritar(filme)} className="h-6 w-6 text-slate-100" /> 
          }
         <img className="rounded" src={url_imagem}alt="" />
         <span className="font-bold text-center line-clamp-1">
